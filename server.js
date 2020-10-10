b90519b14477166e1999f1dff4178d40131e795b
Liz Kiser <superbeffles@gmail.com>
2020-10-10 00:46:38 -0400

    Added cors

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const fs = require('fs');
 
-app.use('/', express.static('public'));
+app.use(cors());
+
 
 let rawdata = fs.readFileSync('budgetdata.json');
 let budget = JSON.parse(rawdata);
-console.log(budget);
-
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
 
 app.get('/budget', (req, res) => {
     res.json(budget);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`API served at http://localhost:${port}`)
 });
